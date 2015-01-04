01e4a51b6558bf7f2b94c5e1d517ec4c3613fd5a
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-01-03 20:21:20 -0600

    improve comments

--- commentAPI.py
+++ commentAPI.py
@@ -5,6 +5,7 @@ from model.news import News
 from model.comment import Comment
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
+from serialize import comments_serialize
 
 commnetParser = reqparse.RequestParser()
 commnetParser.add_argument('token', type=str)
@@ -33,16 +34,12 @@ class CommentAPI(Resource):
 		if title is None or username is None or content is None:
 			abort(400)
 
-		news = News.objects(title=title).only('comments').first()
-		if news is None:
-			abort(400)
-
 		comment = Comment(username=username, content=content)
-		if news.comments is None:
-			news.comments = [comment]
-		else:
-			news.comments.append(comment)
-		news.save()
+		news = News.objects(title=title).only('comments').update_one(push__comments=comment)
+		try:
+			pass
+		except:
+			abort(400)
 
 		return {'status': 'success'}
 
