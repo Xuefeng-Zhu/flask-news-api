c38ea7f4e198744c5447ee9622220a7edbd83c5b
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-01-10 00:01:18 -0600

    I guess simple is the best

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ MONGODB_SETTINGS = {
     'password': '123123'
 }
 SECRET_KEY = 'flask is cool'
-CACHE_TYPE = 'redis'
-CACHE_REDIS_URL = 'redis://redistogo:2e2993c84376806d282fe6fbfa0daa46@mummichog.redistogo.com:10079'
+CACHE_TYPE = 'simple'
+# CACHE_REDIS_URL = 'redis://:123123@pub-redis-17784.us-east-1-2.1.ec2.garantiadata.com:17784/0'
